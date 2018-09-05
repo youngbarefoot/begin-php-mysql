a9f40aa284d149c20d05e2422a1f90bb84485fd7
barefoor <barefoot@whoami.com>
2018-09-05 15:47:36 +0900

    The key/value foreach Statement

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
-    $links = array("www.apress.com", "www.php.net", "www.apache.org");
+    $links = array("The Apache Web Server" => "www.apache.org", "Apress" => "www.apress.com", "The PHP Scripting Language" => "www.php.net");
     echo "<b>Online Resources</b>:<br>";
-    foreach ($links as $link) {
-        echo "<a href=\"http://$link\">$link</a><br>";
+    foreach ($links as $title => $link) {
+        echo "<a href=\"http://$link\">$title</a><br>";
     }
 ?> 
