160be5d05d6e12c8200b03c149c014ff3847ee62
barefoor <barefoot@whoami.com>
2018-09-05 15:28:43 +0900

    The do...while Statement

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php 
-    $linecount = 1;
-    $fh = fopen("sports.txt", "r");
-    while (!feof($th) && $linecount <= 5) {
-        $line = fgets($fh, 4094);
-        echo $line. "<br>";
-        $linecount++;
-    }
+    $count = 11;
+    do {
+        printf("%d squared = %d <br>", $count, pow($count, 2));
+    } while ($count < 10);
 ?> 
